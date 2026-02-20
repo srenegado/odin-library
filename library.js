3fc0f8d42f6831e453c6ec310c3a40dc279015fe
srenegado <scottstationary@gmail.com>
2026-02-20 10:33:39 -0800

    Add display library logic with styling

--- library.js
+++ library.js
@@ -20,3 +20,19 @@ function addBookToLibrary(title, author, pages, read) {
   const aBook = new Book(title, author, pages, read);
   library.push(aBook);
 }
+
+function displayLibrary() {
+  const book_list = document.querySelector(".book-list");
+
+  library.forEach(book => {
+    const book_card = document.createElement("div");
+    book_card.classList.add("book-card");
+    book_card.textContent = book.info();
+    book_list.appendChild(book_card);
+  });
+}
+
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false); // Sample 
+addBookToLibrary("Merlin's Ring", "H. Warner Munn", 384, true);
+
+displayLibrary();
